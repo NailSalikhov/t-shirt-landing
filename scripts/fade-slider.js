dc325c18dbdf457a3c6c64cd3ce4d9becefd83ec
NailSalikhov <salnail35@gmail.com>
2023-01-11 09:30:06 +0400

    Added filter.js/Done with main work

--- scripts/fade-slider.js
+++ scripts/fade-slider.js
@@ -1,18 +1,15 @@
 (function () {
   const slides = document.querySelectorAll(".fade-slider__item");
-  //   const activeClass = document.querySelector(".fade-slider__item_visible");
- const activeClass = "fade-slider__item_visible";
+  const activeClass = "fade-slider__item_visible";
   let index = 0;
-  
-  
+
   setInterval(function () {
     slides[index].classList.remove(activeClass);
     index++;
-    
+
     if (index + 1 > slides.length) {
-    index = 0;
+      index = 0;
     }
     slides[index].classList.add(activeClass);
-    console.log(index);
   }, 5000);
-}());
+})();
